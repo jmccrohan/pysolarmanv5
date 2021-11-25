9eda23104241d485ab791793fd601516d55a22e5
Jonathan McCrohan <jmccrohan@gmail.com>
2021-11-25 23:49:30 +0000

    Update client_example.py to demonstrate new functionality

--- examples/client_example.py
+++ examples/client_example.py
@@ -2,18 +2,32 @@
 from pysolarmanv5.pysolarmanv5 import PySolarmanV5
 
 def main():
-    """Create new PySolarman instance, using IP address and S/N of data logger"""
+    """Create new PySolarman instance, using IP address and S/N of data logger
+
+    Only IP address and S/N of data logger are mandatory parameters. If port,
+    mb_slave_id, and verbose are omitted, they will default to 8899, 1 and 0
+    respectively.
+    """
     modbus = PySolarmanV5('192.168.1.24', 123456789, port = 8899, mb_slave_id = 1, verbose = 1)
 
-    """Query two different input registers, of varying sizes"""
-    print(modbus.read_input_registers(register_addr = 33022, quantity = 1))
-    
-    print(modbus.read_input_registers(register_addr = 33035, quantity = 1, scale = 0.1))
-    
-    print(modbus.read_input_registers(register_addr = 33079, quantity = 2,
-        signed = 1))
+    """Query six input registers, results as a list"""
+    print(modbus.read_input_registers(register_addr = 33022, quantity = 6))
 
-    print(modbus.read_holding_registers(register_addr = 43000, quantity = 1))
+    """Query six holding registers, results as list"""
+    print(modbus.read_holding_registers(register_addr = 43000, quantity = 6))
+
+    """Query single input register, result as an int"""
+    print(modbus.read_input_register_formatted(register_addr = 33035, quantity = 1))
+    
+    """Query single input register, apply scaling, result as a float"""
+    print(modbus.read_input_register_formatted(register_addr = 33035, quantity = 1, scale = 0.1))
+    
+    """Query two input registers, shift first register up by 16 bits, result as a signed int, """
+    print(modbus.read_input_register_formatted(register_addr = 33079, quantity = 2, signed = 1))
+
+    """Query single holding register, apply bitmask and bitshift left (extract bit1 from register)"""
+    print(modbus.read_holding_register_formatted(register_addr = 43110, quantity = 1, bitmask = 0x2, bitshift = 1))
+    
 
 if __name__ == "__main__":
     main()
