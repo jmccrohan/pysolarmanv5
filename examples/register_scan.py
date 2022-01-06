6879e32c7f9a4b67c2b2bbba2cde2dd0197d4b78
Jonathan McCrohan <jmccrohan@gmail.com>
2022-01-06 22:23:56 +0000

    Add register_scan example
    Thanks to @NosIreland for the original code.

---
+++ examples/register_scan.py
@@ -0,0 +1,27 @@
+""" Scan Modbus registers to find valid registers"""
+from pysolarmanv5.pysolarmanv5 import PySolarmanV5, V5FrameError
+import umodbus.exceptions
+
+def main():
+    modbus = PySolarmanV5("192.168.1.24", 123456789, port=8899, mb_slave_id=1, verbose=0)
+ 
+    print("Scanning input registers")
+    for x in range(30000, 39999):
+        try:
+            val = modbus.read_input_registers(register_addr=x, quantity=1)[0]
+            print(f"Register: {x:05}\t\tValue: {val:05} ({val:#06x})")
+        except (V5FrameError, umodbus.exceptions.IllegalDataAddressError):
+            continue
+    print("Finished scanning input registers")
+
+    print("Scanning holding registers")
+    for x in range(40000, 49999):
+        try:
+            val = modbus.read_holding_registers(register_addr=x, quantity=1)[0]
+            print(f"Register: {x:05}\t\tValue: {val:05} ({val:#06x})")
+        except (V5FrameError, umodbus.exceptions.IllegalDataAddressError):
+            continue
+    print("Finished scanning holding registers")
+
+if __name__ == "__main__":
+    main()
