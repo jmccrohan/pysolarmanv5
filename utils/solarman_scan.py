2391ec4b1602769fee1e3c8c30389268f45d8b65
Jonathan McCrohan <jmccrohan@gmail.com>
2022-06-21 00:44:06 +0100

    Add solarman_scan utility

---
+++ utils/solarman_scan.py
@@ -0,0 +1,26 @@
+""" Scan local network for Solarman data loggers """
+
+import socket
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.settimeout(1.0)
+
+    request = "WIFIKIT-214028-READ"
+    address = ("<broadcast>", 48899)
+    
+    sock.sendto(request.encode(), address)
+    while True:
+        try:
+            data = sock.recv(1024)
+        except socket.timeout:
+            break
+        keys = dict.fromkeys(['ipaddress', 'mac', 'serial'])
+        values = data.decode().split(",")
+        result = dict(zip(keys, values))
+        print(result)
+
+if __name__ == "__main__":
+    main()
