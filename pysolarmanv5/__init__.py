19d9fce6a7995f4b920cab455e1bc3027e667bb4
Jonathan McCrohan <jmccrohan@gmail.com>
2022-10-21 23:46:32 +0100

    Add missing exception import

--- pysolarmanv5/__init__.py
+++ pysolarmanv5/__init__.py
@@ -1,9 +1,7 @@
 from pysolarmanv5.pysolarmanv5 import PySolarmanV5
 from pysolarmanv5.pysolarmanv5 import V5FrameError
+from pysolarmanv5.pysolarmanv5 import NoSocketAvailableError
 
 name = "pysolarmanv5"
 
-__all__ = [
-        "PySolarmanV5",
-        "V5FrameError"
-        ]
+__all__ = ["PySolarmanV5", "V5FrameError", "NoSocketAvailableError"]
