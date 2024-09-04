ed578d10db2c3d0d1b259bfd35568a2fa3aa22e4
githubDante <github@dantetk.org>
2024-09-04 19:25:07 +0300

    Updated tests
    - random exception code when the requested register address > 4000
    - separated exception handling
    - increased socket timeout for the test due to ConnectionReset/Emtpy queue errors while the server is sending the bunch of invalid packets

--- tests/test_solarman.py
+++ tests/test_solarman.py
@@ -1,7 +1,10 @@
 import time
 import logging
+
+import pytest
+
 from setup_test import SolarmanServer, AioSolarmanServer
-from pysolarmanv5 import PySolarmanV5, NoSocketAvailableError
+from pysolarmanv5 import PySolarmanV5, NoSocketAvailableError, V5FrameError
 
 log = logging.getLogger()
 # server = SolarmanServer('127.0.0.1', 8899)
@@ -10,7 +13,7 @@ server = AioSolarmanServer("127.0.0.1", 8899)
 
 def test_sync():
     solarman = PySolarmanV5(
-        "127.0.0.1", 2612749371, auto_reconnect=True, verbose=True, socket_timeout=2
+        "127.0.0.1", 2612749371, auto_reconnect=True, verbose=True, socket_timeout=5
     )
     res = solarman.read_holding_registers(20, 4)
     log.debug(f"[Sync-HOLDING] Logger response: {res}")
@@ -31,5 +34,22 @@ def test_sync():
         res = solarman.read_input_registers(60, 10)
     log.debug(f"[Sync-INPUT] Logger response: {res}")
     assert len(res) == 10
+
     solarman.disconnect()
+    time.sleep(0.6)
+    assert solarman._reader_thr.is_alive() is False
     log.debug("[Sync] Disconnected!!!")
+
+    log.debug("[Sync] ===== Starting exception test =====")
+    solarman._reconnect()
+
+    with pytest.raises(V5FrameError, match="V5 Modbus EXCEPTION") as e_info:
+        res = solarman.read_holding_registers(4500, 4)
+
+    assert e_info.type is V5FrameError
+    log.debug(f"[SyncException] {e_info}")
+    try:
+        solarman.disconnect()
+    except:
+        pass
+
