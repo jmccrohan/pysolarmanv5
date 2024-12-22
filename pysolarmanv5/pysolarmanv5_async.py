8b23962d2b5a6000e041e54064340c0467567be8
David Rapan <david@rapan.cz>
2024-12-22 15:18:29 +0100

    feat: Add V5_HANDSHAKE handling for server mode

--- pysolarmanv5/pysolarmanv5_async.py
+++ pysolarmanv5/pysolarmanv5_async.py
@@ -163,12 +163,19 @@ class PySolarmanV5Async(PySolarmanV5):
 
     async def _handle_protocol_frame(self, frame):
         """
-        Handles protocol frames with control code 0x4710 (heartbeat frame).
+        Handles protocol frames with control code 0x4110 (handshake) and 0x4710 (heartbeat).
         """
+        response_frame = None
+
+        if frame[4] == 0x41:
+            self.log.debug("[%s] V5_HANDSHAKE: %s", self.serial, frame.hex(" "))
+            response_frame = self._v5_time_response_frame(frame)
+            self.log.debug("[%s] V5_HANDSHAKE RESP: %s", self.serial, response_frame.hex(" "))
         if frame.startswith(self.v5_start + b"\x01\x00\x10\x47"):
             self.log.debug("[%s] V5_HEARTBEAT: %s", self.serial, frame.hex(" "))
             response_frame = self._v5_time_response_frame(frame)
             self.log.debug("[%s] V5_HEARTBEAT RESP: %s", self.serial, response_frame.hex(" "))
+        if response_frame:
             try:
                 self.writer.write(response_frame)
                 await self.writer.drain()
